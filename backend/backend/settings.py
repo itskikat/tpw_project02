a9cc1c09c833a28e3c8b478f535f918b2563bbe3
l-sousa <48656255+l-sousa@users.noreply.github.com>
2021-07-01 19:59:22 +0100

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,6 +25,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:4200',
+    'https://tpw-angular-frontend.herokuapp.com'
 ]
 
 CORS_ALLOW_HEADERS = [
